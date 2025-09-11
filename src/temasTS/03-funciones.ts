e1ced2bfd12028342dcd11e72f5d2aaf0cb8ef78
Ivan-Lopez <hoscarivan615@gmail.com>
2025-09-10 20:49:04 -0600

    importaciones y exportaciones

--- src/temasTS/03-funciones.ts
+++ src/temasTS/03-funciones.ts
@@ -15,4 +15,35 @@ console.log(resultado2)
 
 //funciones de flecha
 
-const addNumberArrow=(a:number, d:number):string->[return ´´]
+const addNumberArrow = (a: number, b: number): string => {
+  return `${a + b}`;
+};
+interface Mascotas{
+    nombre:string,
+    edad:number,
+    raza:string,
+    sexo:string,
+    vacunado:boolean,
+    mostrarEdad:()=>void
+}
+
+function mostrarMascotas(mascota: Mascotas, x: number): void {
+    mascota.edad += x;
+    console.log(mascota);
+    console.log(mascota.mostrarEdad());
+}
+
+const nuevaMascota: Mascotas={
+    nombre: "firulais",
+    
+    edad: 5,
+    raza: "pastor aleman",
+    sexo:"femenino",
+    vacunado: true,
+    mostrarEdad() {
+        console.log(`la edad de ${this.nombre} es ${this.edad}`);
+    }
+}
+
+mostrarMascotas(nuevaMascota, 2);
+
